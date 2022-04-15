0bdc07a40b66e9613031b05bf4d7d0a32b739c83
fernandonunex <fer.bsof@outlook.com>
2022-04-14 22:12:02 -0500

    Code and test, all good.

--- prime_static_typing.py
+++ prime_static_typing.py
@@ -26,19 +26,27 @@ def is_prime_v2(number: int) -> bool:
         bool: Returs True if is prime and False if is not prime
     """
     factors_list = [factor for factor in range(
-        1, number+1) if number % factor == 2]
+        2, number) if number % factor == 0]
 
-    return len(factors_list) == 2
+    return len(factors_list) == 0
 
 
 def run():
-    #Test for function V_1 (for and ifs)
+    # Test for function V_1 (for and ifs)
+    print("\nFor function V_1 (for and ifs)")
     t0 = time.time()
-    print(is_prime(5780))
+    # print(is_prime(5780107))   #Number that is not prime
+    print(is_prime(7919))  # Number that is prime
     t1 = time.time()
     print("Time elapsed: ", t1 - t0)  # CPU seconds elapsed (floating point)
 
-    #Test for function V_2 (List comprenhensions)
+    # Test for function V_2 (List comprenhensions)
+    print("\nFor function V_2 (List comprenhensions)")
+    t0 = time.time()
+    # print(is_prime_v2(5780107))    #Number that is not prime
+    print(is_prime_v2(7919))  # Number that is prime
+    t1 = time.time()
+    print("Time elapsed: ", t1 - t0)  # CPU seconds elapsed (floating point)
 
 
 if __name__ == '__main__':
