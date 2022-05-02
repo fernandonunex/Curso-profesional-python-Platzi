ca5131b1ca2bd20e02cb57abad964c77c6b5b1b6
fernandonunex <fer.bsof@outlook.com>
2022-05-02 15:29:55 -0500

    Challenge exercise removing duplicates from  a list using sets

--- sets.py
+++ sets.py
@@ -8,10 +8,17 @@ def remove_duplicates(some_list):
     return without_duplicates
 
 
+def remove_duplicates_sets(some_list):
+    return list(set(some_list))
+
+
 def run():
     random_list = [1, 1, 2, 2, 4]
     print(remove_duplicates(random_list))
 
+    random_list = [1, 1, 2, 2, 4]
+    print(remove_duplicates_sets(random_list))
+    
 
 if __name__ == "__main__":
-        run() 
+    run()
